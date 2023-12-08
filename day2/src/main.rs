91d2cba20fdff1c6aec7b97d034cc7d56fc0b98c
Gabriel <gabrielsouza628@gmail.com>
2023-12-07 23:43:06 -0300

    feat: day2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,14 @@
+use std::cmp;
 use std::fs;
 
 fn main() {
     let binding: String = fs::read_to_string("input.txt").expect("Should be able to read file");
+
+    part1(binding.clone());
+    part2(binding.clone());
+}
+
+fn part1(binding: String) {
     let games = binding.split("\n").into_iter();
 
     let mut count = 0;
@@ -18,6 +25,41 @@ fn main() {
     }
 
     println!("Result - {}", count)
+}
+
+fn part2(binding: String) {
+    let mut sum: i32 = 0;
+
+    for game in binding.lines() {
+        let mut red_min_value: i32 = i32::default();
+        let mut blue_min_value: i32 = i32::default();
+        let mut green_min_value: i32 = i32::default();
+
+        let (_, right) = game.split_at(game.rfind(":").unwrap());
+        let right = right.strip_prefix(":").unwrap();
+
+        for a in right.trim().split("; ") {
+            let aux: Vec<_> = a.trim_start().split(", ").collect();
+
+            for color in aux {
+                let splitted: Vec<&str> = color.split(" ").collect();
+                let quantity = splitted[0].parse::<i32>().unwrap();
+                let color = splitted[1];
+
+                match color {
+                    "red" => red_min_value = cmp::max(red_min_value, quantity),
+                    "green" => green_min_value = cmp::max(green_min_value, quantity),
+                    "blue" => blue_min_value = cmp::max(blue_min_value, quantity),
+                    &_ => unreachable!(),
+                };
+            }
+        }
+
+        let power = red_min_value * blue_min_value * green_min_value;
+        sum += power
+    }
+
+    println!("Result Part 2 - {}", sum)
 }
 
 fn is_valid(bag: &&str) -> bool {
@@ -41,3 +83,5 @@ fn is_color_up_to_max(item: &str) -> bool {
         &_ => unreachable!(),
     };
 }
+
+fn parse_game() {}
